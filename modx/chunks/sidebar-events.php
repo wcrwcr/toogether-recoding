31d7d5bbc8a26cd60d7455b7e1b131f44b4284e2
viacheslav <tsstsstosser@gmail.com>
2016-08-21 13:48:50 +0200

    contacts and programs

---
+++ modx/chunks/sidebar-events.php
@@ -0,0 +1,7 @@
+<div class="another-info-cont">
+    <h4>акции</h4>
+    <div class="info-cont-img">
+        <img src="/assets/images/contact/another/our-brochure.jpg" alt="img">
+        <a href="#" class="info-button">узнать подробнее</a>
+    </div>
+</div>
---
+++ modx/chunks/sidebar-join.php
@@ -0,0 +1,11 @@
+<div class="another-info-cont">
+    <h4>присоединяйся к нам!</h4>
+    <ul class="to-join">
+        <li><a href="#"><img src="/assets/images/contact/another/join/twitter.png" alt="img"></a></li>
+        <li><a href="#"><img src="/assets/images/contact/another/join/youtube.png" alt="img"></a></li>
+        <li><a href="#"><img src="/assets/images/contact/another/join/fb.png" alt="img"></a></li>
+        <li><a href="#"><img src="/assets/images/contact/another/join/gplus.png" alt="img"></a></li>
+        <li><a href="#"><img src="/assets/images/contact/another/join/instagram.png" alt="img"></a></li>
+        <li><a href="#"><img src="/assets/images/contact/another/join/vk.png" alt="img"></a></li>
+    </ul>
+</div>
