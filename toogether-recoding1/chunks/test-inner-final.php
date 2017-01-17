e132d03b5cadc143d057d1365b46c5d3a866d117
Black_Moon <nickandreyuser@gmail.com>
2017-01-17 01:44:01 +0100

    photo-group hide; акции; галерея; тест; спрайты
    photo-group hide -
    https://app.asana.com/0/240035665589319/247602565218458
    
    акции - https://app.asana.com/0/240035665589319/244327697536722
    
    Поведение галереи на главной -
    https://app.asana.com/0/240035665589319/246968746358905
    
    страница тест - https://app.asana.com/0/240035665589319/236677580711193
    
    спрайты (весь сайт) -
    https://app.asana.com/0/240035665589319/244335332279010

---
+++ toogether-recoding1/chunks/test-inner-final.php
@@ -0,0 +1,18 @@
+<div id ='test-result-holder' style='display:none'>
+    <div class="ans-your-level">
+        <h2 class="ans-title">Ты - парк Лазеньковски! </h2>
+        <div class="ans-photo">
+                <img src="/assets/images/test/polishGarden/bg-1.jpg" alt="">
+            </div>
+        <p class="ans-subscr">
+                CJ Together продолжает психоанализ самых странных эпизодов нашей жизни (в предыдущие разы, к примеру, мы искали в себе пережитки «совка» и остатки украинских райцентров). Сегодня, продолжая работу с ностальгией по нашему общему детству и дурацким вещам в нем, мы решили выяснить, кто же является сухой «Мивиной», а кто — сменка для обуви на физре.</p>
+        <div class="test-soc">
+            <span class="soc-title">Поделится результатом</span>
+            <a href="#"><img src="assets/images/test/polishGarden/share-soc/fb.png" alt="img"></a>
+            <a href="#"><img src="assets/images/test/polishGarden/share-soc/vk.png" alt="img"></a>
+            <a href="#"><img src="assets/images/test/polishGarden/share-soc/twit.png" alt="img"></a>
+            <a href="#"><img src="assets/images/test/polishGarden/share-soc/drd.png" alt="img"></a>
+        </div>
+
+    </div>
+</div>
